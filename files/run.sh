ee05da61ada0febf26aa98da84299806165b9c0a
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-11-02 18:43:39 -0700

    do not expose solr outside container and write json on disk

--- files/run.sh
+++ files/run.sh
@@ -9,9 +9,9 @@ rm /data/solr-6.2.1/server/solr/golr/conf/managed-schema
 cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-app/conf/golr-views/oban-config.yaml -o /data/solr-6.2.1/server/solr/golr/conf/schema.xml"
 wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz 
 cd /data/ && tar xzfv scigraph.tgz
-mkdir -p /data/json
-/data/solr-6.2.1/bin/solr start
-cd /data/golr-loader && java -Xmx50G -Dlog4j.configuration=file:/data/log4j.properties -jar target/golr-loader-0.0.1-SNAPSHOT-jar-with-dependencies.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader -o /data/json/ -s http://localhost:8983/solr/golr
+mkdir -p /solr/json
+/data/solr-6.2.1/bin/solr start -m 10g
+cd /data/golr-loader && java -Xmx50G -Dlog4j.configuration=file:/data/log4j.properties -jar target/golr-loader-0.0.1-SNAPSHOT-jar-with-dependencies.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader -o /solr/json/ -s http://localhost:8983/solr/golr
 /data/solr-6.2.1/bin/solr stop
 cd /data/solr-6.2.1/server/solr && tar czfv golr.tgz golr/
 cp /data/solr-6.2.1/server/solr/golr.tgz /solr
