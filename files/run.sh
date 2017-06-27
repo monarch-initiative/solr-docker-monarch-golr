087a32d53f971e37bd373bdf06412b1b4e1c11d9
Kent Shefchek <kshefchek@gmail.com>
2017-06-27 11:50:44 -0700

    remove -o option

--- files/run.sh
+++ files/run.sh
@@ -10,8 +10,8 @@ cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexe
 wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz
 cd /data/ && tar xzfv scigraph.tgz
 mkdir -p /solr/json
-/data/solr-6.2.1/bin/solr start -m 20g
-cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -o /solr/json/ -s http://localhost:8983/solr/golr -d
+/data/solr-6.2.1/bin/solr start -m 10g
+cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr -d
 /data/solr-6.2.1/bin/solr stop || true
 #rm -rf /solr/json
 cd /data/solr-6.2.1/server/solr && tar cfv golr.tar golr/
