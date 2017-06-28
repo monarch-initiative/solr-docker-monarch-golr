e2519a342cb535d636786dc087a80eac85c1ea77
Kent Shefchek <kshefchek@gmail.com>
2017-06-28 11:23:39 -0700

    test failing query

--- files/run.sh
+++ files/run.sh
@@ -11,7 +11,7 @@ wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static
 cd /data/ && tar xzfv scigraph.tgz
 mkdir -p /solr/json
 /data/solr-6.2.1/bin/solr start -m 10g
-cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
+cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/failing-queries/ -s http://localhost:8983/solr/golr
 /data/solr-6.2.1/bin/solr stop || true
 #rm -rf /solr/json
 cd /data/solr-6.2.1/server/solr && tar cfv golr.tar golr/
