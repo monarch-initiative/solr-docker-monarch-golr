416e659ad30f9ccf5bf6bc128ff368eb9f7e40c8
Kent Shefchek <kshefchek@gmail.com>
2017-07-01 12:55:36 -0700

    mv config after making core

--- files/run.sh
+++ files/run.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
 /data/solr-6.2.1/bin/solr start
 /data/solr-6.2.1/bin/solr create -c golr
 /data/solr-6.2.1/bin/solr stop
@@ -10,6 +9,7 @@ rm /data/solr-6.2.1/server/solr/golr/conf/managed-schema
 cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-app/conf/golr-views/oban-config.yaml -o /data/solr-6.2.1/server/solr/golr/conf/schema.xml"
 wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz
 cd /data/ && tar xzfv scigraph.tgz
+mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
 mkdir -p /solr/json
 /data/solr-6.2.1/bin/solr start -m 10g
 cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
