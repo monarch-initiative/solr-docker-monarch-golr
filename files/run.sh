992e33a8ecbd1eb8a3b46314ab6f6b313b52ae64
Kent Shefchek <kshefchek@gmail.com>
2017-07-06 21:56:36 -0700

    add apoc to graph

--- files/run.sh
+++ files/run.sh
@@ -9,6 +9,7 @@ rm /data/solr-6.2.1/server/solr/golr/conf/managed-schema
 cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/monarch-app/conf/golr-views/oban-config.yaml -o /data/solr-6.2.1/server/solr/golr/conf/schema.xml"
 wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz
 cd /data/ && tar xzfv scigraph.tgz
+mkdir /data/graph/plugins && mv /data/apoc-3.0.8.6-all.jar /data/graph/plugins/
 mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
 mkdir -p /solr/json
 /data/solr-6.2.1/bin/solr start -m 10g
