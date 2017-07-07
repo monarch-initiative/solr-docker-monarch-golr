d6647e04370ed35b7041daefaa59cc3061bc23f9
kshefchek <kshefchek@gmail.com>
2017-07-07 15:41:03 -0700

    tweaking mem settings

--- files/run.sh
+++ files/run.sh
@@ -12,7 +12,7 @@ cd /data/ && tar xzfv scigraph.tgz
 mkdir /data/graph/plugins && mv /data/apoc-3.0.8.6-all.jar /data/graph/plugins/
 mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
 mkdir -p /solr/json
-/data/solr-6.2.1/bin/solr start -m 20g
+/data/solr-6.2.1/bin/solr start -m 10g
 cd /data/golr-loader && java -Xmx100G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
 /data/solr-6.2.1/bin/solr stop || true
 #rm -rf /solr/json
