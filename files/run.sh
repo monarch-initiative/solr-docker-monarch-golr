9b7edde248e1dea17b40271a5a3684a6cd5359b7
Kent Shefchek <kshefchek@gmail.com>
2020-02-18 09:44:39 -0800

    add port to solr cmds

--- files/run.sh
+++ files/run.sh
@@ -3,8 +3,8 @@
 set -e
 
 /data/solr-6.2.1/bin/solr start
-/data/solr-6.2.1/bin/solr create -c golr
-/data/solr-6.2.1/bin/solr stop
+/data/solr-6.2.1/bin/solr -p 8983 create -c golr
+/data/solr-6.2.1/bin/solr -p 8983 stop
 rm /data/solr-6.2.1/server/solr/golr/conf/managed-schema
 cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/golr-config.yaml -o /data/solr-6.2.1/server/solr/golr/conf/schema.xml"
 # Set jetty idle timeout to 200 seconds
@@ -15,7 +15,6 @@ mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
 mkdir -p /solr/json
 /data/solr-6.2.1/bin/solr start -m 10g
 cd /data/golr-loader && java -Xmx300G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
-#rm -rf /solr/json
 rm /data/scigraph.tgz
 rm -rf /data/graph/
 curl http://localhost:8983/solr/golr/update?optimize=true
