63e72e52a84cdcb57cff6bdf10a60988e105cfd0
Kent Shefchek <kshefchek@gmail.com>
2020-02-18 09:51:56 -0800

    adds monarch user so volume does not require root priv on host

--- Dockerfile
+++ Dockerfile
@@ -4,35 +4,40 @@ RUN rm /bin/sh && ln -s /bin/bash /bin/sh
 
 ARG CURIE_MAP='https://archive.monarchinitiative.org/beta/translationtable/curie_map.yaml'
 
-RUN adduser --disabled-password --uid 1006 monarch
-
 # Install git and wget
 RUN apt-get -y update && apt-get install -y git wget
 
-# Define working directory.
-WORKDIR /data
-ADD files/functions.inc /data/
-ADD files/logback.xml /data/
-ADD files/run.sh /data/
-ADD files/solrconfig.xml /data/
-ADD files/golr-config.yaml /data/
-
-RUN git clone https://github.com/SciGraph/SciGraph.git /data/scigraph
-RUN git clone https://github.com/SciGraph/golr-loader.git /data/golr-loader
-RUN git clone https://github.com/monarch-initiative/monarch-cypher-queries.git /data/monarch-cypher-queries
-RUN git clone https://github.com/berkeleybop/golr-schema /data/golr-schema
-
-RUN cd /data/scigraph && mvn install -DskipTests -DskipITs
-RUN cd /data/golr-loader && mvn install -Dmaven.test.skip
-RUN cd /data/monarch-cypher-queries && mvn install
-RUN cd /data/golr-schema && mvn install
-
-RUN wget http://archive.apache.org/dist/lucene/solr/6.2.1/solr-6.2.1.tgz -P /data/
-RUN cd /data && tar xzfv /data/solr-6.2.1.tgz
-
-RUN cd /data && source /data/functions.inc && getGraphConfiguration /data/graph $CURIE_MAP > graph.yaml
-
+RUN adduser --disabled-password --uid 1006 monarch
 USER monarch
+
+ARG WORKDIR=/home/monarch/data
+
+ENV MAVEN_CONFIG "$WORKDIR/.m2"
+
 VOLUME /solr
 
-CMD /data/run.sh
+# Define working directory.
+RUN mkdir $WORKDIR
+WORKDIR $WORKDIR
+ADD files/functions.inc $WORKDIR
+ADD files/logback.xml $WORKDIR
+ADD files/run.sh $WORKDIR
+ADD files/solrconfig.xml $WORKDIR
+ADD files/golr-config.yaml $WORKDIR
+
+RUN git clone https://github.com/SciGraph/SciGraph.git $WORKDIR/scigraph
+RUN git clone https://github.com/SciGraph/golr-loader.git $WORKDIR/golr-loader
+RUN git clone https://github.com/monarch-initiative/monarch-cypher-queries.git $WORKDIR/monarch-cypher-queries
+RUN git clone https://github.com/berkeleybop/golr-schema $WORKDIR/golr-schema
+
+RUN cd $WORKDIR/scigraph && mvn install -DskipTests -DskipITs
+RUN cd $WORKDIR/golr-loader && mvn install -Dmaven.test.skip
+RUN cd $WORKDIR/monarch-cypher-queries && mvn install
+RUN cd $WORKDIR/golr-schema && mvn install
+
+RUN wget http://archive.apache.org/dist/lucene/solr/6.2.1/solr-6.2.1.tgz -P $WORKDIR
+RUN cd $WORKDIR && tar xzfv $WORKDIR/solr-6.2.1.tgz
+
+RUN source $WORKDIR/functions.inc && getGraphConfiguration $WORKDIR/graph $CURIE_MAP > graph.yaml
+
+CMD /home/monarch/data/run.sh
--- files/run.sh
+++ files/run.sh
@@ -2,23 +2,26 @@
 
 set -e
 
-/data/solr-6.2.1/bin/solr start
-/data/solr-6.2.1/bin/solr -p 8983 create -c golr
-/data/solr-6.2.1/bin/solr -p 8983 stop
-rm /data/solr-6.2.1/server/solr/golr/conf/managed-schema
-cd /data/golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c /data/golr-config.yaml -o /data/solr-6.2.1/server/solr/golr/conf/schema.xml"
+WORKDIR=/home/monarch/data
+
+cd $WORKDIR
+
+./solr-6.2.1/bin/solr start -p 8983
+./solr-6.2.1/bin/solr create -p 8983 -c golr
+./solr-6.2.1/bin/solr stop -p 8983
+rm ./solr-6.2.1/server/solr/golr/conf/managed-schema
+cd ./golr-schema && mvn exec:java -Dexec.mainClass="org.bbop.cli.Main" -Dexec.args="-c $WORKDIR/golr-config.yaml -o $WORKDIR/solr-6.2.1/server/solr/golr/conf/schema.xml"
 # Set jetty idle timeout to 200 seconds
-sed -i 's/<Set name="idleTimeout"><Property name="solr.jetty.http.idleTimeout" default="50000"\/><\/Set>/<Set name="idleTimeout"><Property name="solr.jetty.http.idleTimeout" default="200000"\/><\/Set>/' /data/solr-6.2.1/server/etc/jetty-http.xml
-wget -O /data/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz
-cd /data/ && tar xzfv scigraph.tgz
-mv /data/solrconfig.xml  /data/solr-6.2.1/server/solr/golr/conf/
-mkdir -p /solr/json
-/data/solr-6.2.1/bin/solr start -m 10g
-cd /data/golr-loader && java -Xmx300G -Dlogback.configurationFile=file:/data/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g /data/graph.yaml -q /data/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
-rm /data/scigraph.tgz
-rm -rf /data/graph/
+sed -i 's/<Set name="idleTimeout"><Property name="solr.jetty.http.idleTimeout" default="50000"\/><\/Set>/<Set name="idleTimeout"><Property name="solr.jetty.http.idleTimeout" default="200000"\/><\/Set>/' $WORKDIR/solr-6.2.1/server/etc/jetty-http.xml
+wget -O $WORKDIR/scigraph.tgz http://scigraph-data-dev.monarchinitiative.org/static_files/scigraph.tgz
+cd $WORKDIR && tar xzfv scigraph.tgz
+mv ./solrconfig.xml  ./solr-6.2.1/server/solr/golr/conf/
+./solr-6.2.1/bin/solr start -p 8983 -m 10g
+cd ./golr-loader && java -Xmx275G -Dlogback.configurationFile=file:$WORKDIR/logback.xml -jar target/golr-loader-0.0.1-SNAPSHOT.jar -g $WORKDIR/graph.yaml -q $WORKDIR/monarch-cypher-queries/src/main/cypher/golr-loader/ -s http://localhost:8983/solr/golr
+rm $WORKDIR/scigraph.tgz
+rm -rf $WORKDIR/graph/
 curl http://localhost:8983/solr/golr/update?optimize=true
-/data/solr-6.2.1/bin/solr stop || true
-cd /data/solr-6.2.1/server/solr && tar cfv golr.tar golr/
-rm -rf /data/solr-6.2.1/server/solr/golr
-mv /data/solr-6.2.1/server/solr/golr.tar /solr
+$WORKDIR/solr-6.2.1/bin/solr stop || true
+cd $WORKDIR/solr-6.2.1/server/solr && tar cfv golr.tar golr/
+rm -rf $WORKDIR/solr-6.2.1/server/solr/golr
+mv $WORKDIR/solr-6.2.1/server/solr/golr.tar /solr
